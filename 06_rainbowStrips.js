ce930b807b85992246af27226df15fd21503351b
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-03-19 19:26:44 -0700

    Update pattern 06

--- 06_rainbowStrips.js
+++ 06_rainbowStrips.js
@@ -1,1 +1,10 @@
-file:///home/alejandro/repositories/alejandrosame/pixelblaze-animations/editor/base/index.html
+export function beforeRender(delta) {
+  hl = pixelCount / 2;
+  t = time(0.01);
+  v = 0.01 + wave(time(0.07));
+}
+
+export function render(index) {
+  c1 = 1 - abs(index - hl) / hl;
+  hsv(c1 + t, 1, v);
+}
